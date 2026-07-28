b6849d5e242c034c1f7f68b484601af1d500ea6d
NLTH-mal <63522482+NLTH-mal@users.noreply.github.com>
2026-07-28 14:26:12 -0400

    Update ansatz.py

--- src/ansatz.py
+++ src/ansatz.py
@@ -1,1 +1,18 @@
+import sympy
 
+import cirq
+
+
+def create_ansatz():
+    qubits = cirq.LineQubit.range(4)
+    symbols = [sympy.Symbol(f'theta_{i}') for i in range(4)]
+
+    circuit = cirq.Circuit()
+    for i in range(4):
+        circuit.append(cirq.rx(symbols[i])(qubits[i]))
+    # Add some entanglement
+    circuit.append(cirq.CNOT(qubits[0], qubits[1]))
+    circuit.append(cirq.CNOT(qubits[2], qubits[3]))
+    return circuit
+
+print(create_ansatz())
