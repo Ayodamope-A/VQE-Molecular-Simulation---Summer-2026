9b05a047285d4cdfbbc126e71c1a0d585be6cda6
Ayodamope-A <ayodamopeayodamope@gmail.com>
2026-07-29 15:32:14 -0400

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("results/results.csv")
+experiment_data = pd.read_csv("results/results.csv")
 
-table = df[["shots'. "trial", "exact_energy","vqe_energy", "error"]]
+table = experiment_data[["shots'. "trial", "exact_energy","vqe_energy", "error"]]
 
 print(table)
 
-average_error = df.groupby("shots")["error"].mean()
+average_error = experiment_data.groupby("shots")["error"].mean()
 
-plt.plot(df["Shout Count"], df["Percent Error"], marker="x")
+plt.plot(experiment_data["Shout Count"], df["Percent Error"], marker="x")
 plt.xlabel("Number of Shots")
 plt.ylabel("Absolute Error")
 plt.title("Number of Shots compared with energy error")
