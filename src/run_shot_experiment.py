be7f1a648993ebac15396e6ec2b1c7a49a90cf7d
Ayodamope-A <ayodamopeayodamope@gmail.com>
2026-07-29 15:41:38 -0400

    Update run_shot_experiment.py

--- src/run_shot_experiment.py
+++ src/run_shot_experiment.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from vqe import vqe
 
 shot_counts = [100, 500, 1000, 5000, 10000]
 trial_number = 3
 results = []
+vqe_energy = vqe(H_sparse)
 percent_error = abs(vqe_energy - exact_energy)
 
 
@@ -19,5 +21,5 @@ for shots in shot_counts:
         results.append(dictionary)
         print("Shot Count:", shots, "Trial:", trial + 1)
 
- experiment_data = pd.DataFrame(results)
- experiment_data.to_csv("results/results.csv", index=False)
+experiment_data = pd.DataFrame(results)
+experiment_data.to_csv("results/results.csv", index=False)
