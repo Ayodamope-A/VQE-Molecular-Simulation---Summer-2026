b32b11011f390d08a06845553709749fe68ea975
NLTH-mal <63522482+NLTH-mal@users.noreply.github.com>
2026-07-30 15:08:26 -0400

    Update ansatz.py

--- ansatz.py
+++ ansatz.py
@@ -5,14 +5,22 @@ import cirq
 
 def create_ansatz():
     qubits = cirq.LineQubit.range(4)
-    symbols = [sympy.Symbol(f'theta_{i}') for i in range(4)]
+    symbols = [sympy.Symbol(f'theta_{i}') for i in range(8)]
 
     circuit = cirq.Circuit()
-    for i in range(4):
-        circuit.append(cirq.rx(symbols[i])(qubits[i]))
+    circuit.append(cirq.X(qubits[0]))
+    circuit.append(cirq.X(qubits[1]))
+    for x in range(2):
+            
+        for i in range(4):
+            circuit.append(cirq.ry(symbols[x*4+i])(qubits[i]))
+    
+        circuit.append(cirq.CNOT(qubits[0], qubits[1]))
+        circuit.append(cirq.CNOT(qubits[0], qubits[2]))
+        circuit.append(cirq.CNOT(qubits[1], qubits[2]))
+        circuit.append(cirq.CNOT(qubits[0], qubits[3]))
+        circuit.append(cirq.CNOT(qubits[1], qubits[3]))
+        circuit.append(cirq.CNOT(qubits[2], qubits[3]))
         
-    circuit.append(cirq.CNOT(qubits[0], qubits[1]))
-    circuit.append(cirq.CNOT(qubits[2], qubits[3]))
-    return circuit
+    return circuit, symbols
 
-print(create_ansatz())
