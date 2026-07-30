87317039af0b2b8e02d163c85c0745716f5dce06
NLTH-mal <63522482+NLTH-mal@users.noreply.github.com>
2026-07-30 15:10:34 -0400

    Update vqe.py

--- vqe.py
+++ vqe.py
@@ -2,9 +2,6 @@ import numpy as np
 import cirq
 from scipy.optimize import minimize
 from ansatz import create_ansatz
-from molecule import create_h2_molecule
-from hamiltonian import generate_hamiltonian
-from openfermion.linalg import get_sparse_operator
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -13,7 +10,7 @@ warnings.filterwarnings("ignore")
 def vqe(H_Sparse):
 
     def expectation(params):
-        resolver = cirq.ParamResolver({str(symbols[i]): params[i] for i in range(4)})
+        resolver = cirq.ParamResolver({str(symbols[i]): params[i] for i in range(len(symbols))})
         result = sim.simulate(circuit, resolver)
         psi = result.final_state_vector  # shape: (16,)
         return np.real(psi.conj().T @ (H_Sparse @ psi))
@@ -21,14 +18,6 @@ def vqe(H_Sparse):
     circuit, symbols = create_ansatz()
     sim = cirq.Simulator()
     
-    x0 = np.random.uniform(0, 2*np.pi, 4)
+    x0 = np.random.uniform(0, 2*np.pi, len(symbols))
     res = minimize(expectation, x0=x0, method='COBYLA', options={'maxiter': 300})
     return res.fun
-    
-
-molecule = create_h2_molecule()
-molecule, qubit_hamiltonian = generate_hamiltonian(molecule)
-H_Sparse = get_sparse_operator(qubit_hamiltonian)
-
-
-#print(vqe(H_Sparse))
